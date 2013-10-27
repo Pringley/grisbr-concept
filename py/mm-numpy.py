6f7ad3fb87da8c59c5b5e8fe7558c9e59230acea
Ben Pringle <ben.pringle@gmail.com>
2013-10-27 08:01:10 -0400

    Add numpy multiply

--- py/mm-numpy.py
+++ py/mm-numpy.py
@@ -1,1 +1,25 @@
-import numpy
+from __future__ import print_function
+from sys import argv, stderr, exit
+from numpy import array, dot
+
+def read_toks(fp):
+    return fp.readline().split()
+def read_ints(fp):
+    return map(int, read_toks(fp))
+def read_floats(fp):
+    return map(float, read_toks(fp))
+def read_matrix(fp, rows):
+    return array([read_floats(fp) for i in range(rows)])
+
+if len(argv) != 2:
+    print("Usage: {} <input>".format(argv[0]),
+            file=stderr)
+    exit(1)
+
+filename = argv[1]
+with open(filename) as fp:
+    m, k, n = read_ints(fp)
+    a = read_matrix(fp, m)
+    b = read_matrix(fp, k)
+
+c = dot(a, b)

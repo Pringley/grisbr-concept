14d2ecb1fa76330530baeeaacd24d14356d706a3
Ben Pringle <ben.pringle@gmail.com>
2013-10-27 06:41:07 -0400

    Switch from int to float multiplication

--- c/mm.c
+++ c/mm.c
@@ -1,12 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+/* Allocate memory for an m by n matrix. */
+void alloc_matrix(int m, int n, float ***a);
+
+/* Free memory for an m by n matrix. */
+void free_matrix(int m, float **a);
+
+/* Read an m by n matrix from an open file. */
+void read_matrix(FILE *fp, int m, int n, float **a);
+
+/*
+ * Multiply two matrices.
+ *
+ * a: m by k
+ * b: k by n
+ * c: a * b (result)
+ */
+void mult_matrix(int m, int k, int n, float **a, float **b, float **c);
+
 int main(int argc, char *argv[]) {
-    // Matrix A: M by K
-    // Matrix B: K by N
-    // C = A * B
     int M, K, N;
-    int **A, **B, **C;
-    int i, j, k;
+    float **A = NULL,
+          **B = NULL,
+          **C = NULL;
     FILE *fp;
 
     if (argc != 2) {
@@ -20,43 +37,59 @@ int main(int argc, char *argv[]) {
     fscanf(fp, "%d", &K);
     fscanf(fp, "%d", &N);
 
-    // Read first matrix.
-    A = calloc(M, sizeof(int *));
-    for (i = 0; i < M; ++i) {
-        A[i] = calloc(K, sizeof(int));
-        for (j = 0; j < K; ++j) {
-            fscanf(fp, "%d", &A[i][j]);
-        }
-    }
+    // Allocate memory for matrices.
+    alloc_matrix(M, K, &A);
+    alloc_matrix(K, N, &B);
+    alloc_matrix(M, N, &C);
 
-    // Read second matrix.
-    B = calloc(K, sizeof(int *));
-    for (i = 0; i < K; ++i) {
-        B[i] = calloc(N, sizeof(int));
-        for (j = 0; j < N; ++j) {
-            fscanf(fp, "%d", &B[i][j]);
-        }
-    }
+    // Read matrices from file.
+    read_matrix(fp, M, K, A);
+    read_matrix(fp, K, N, B);
 
     fclose(fp);
 
-    // Allocate third matrix.
-    C = calloc(M, sizeof(int *));
-    for (i = 0; i < M; ++i) {
-        C[i] = calloc(N, sizeof(int));
-        for (j = 0; j < N; ++j) {
-            C[i][j] = 0;
+    // Multiply.
+    mult_matrix(M, K, N, A, B, C);
+
+    free_matrix(M, A);
+    free_matrix(K, B);
+    free_matrix(M, C);
+
+    return EXIT_SUCCESS;
+}
+
+void alloc_matrix(int m, int n, float ***a) {
+    int i;
+    (*a) = (float **) calloc(m, sizeof(float *));
+    for (i = 0; i < m; ++i) {
+        (*a)[i] = (float *) calloc(n, sizeof(float));
+    }
+}
+
+void free_matrix(int m, float **a) {
+    int i;
+    for (i = 0; i < m; ++i) {
+        free(a[i]);
+    }
+    free(a);
+}
+
+void read_matrix(FILE *fp, int m, int n, float **a) {
+    int i, j;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            fscanf(fp, "%f", &a[i][j]);
         }
     }
-
-    // Multiply.
-    for (i = 0; i < M; ++i) {
-        for (j = 0; j < K; ++j) {
-            for(k = 0; k < N; ++k) {
-                C[i][j] += A[i][k] * B[k][j];
+}
+void mult_matrix(int m, int k, int n, float **a, float **b, float **c) {
+    int i, j, h;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < k; ++j) {
+            c[i][j] = 0;
+            for(h = 0; h < n; ++h) {
+                c[i][j] += a[i][h] * b[h][j];
             }
         }
     }
-
-    return EXIT_SUCCESS;
 }
